2d45adebabfd91e21a0f40db25d587235ab6ae1f
Santiago <snvc00@gmail.com>
2020-09-03 14:06:38 -0500

    Add version number

--- src/components/Footer/Footer.component.jsx
+++ src/components/Footer/Footer.component.jsx
@@ -43,6 +43,7 @@ export default function Footer(props) {
               </a>
             </div>
           </div>
+            <p className="version-number">{props.appData.version}</p>
         </div>
       </footer>
     </div>
